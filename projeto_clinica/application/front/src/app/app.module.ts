f55c412d69384eb3e2188457987076adb2b30a58
Marco Antônio Almeida <marco.almeida@sou.unaerp.edu.br>
2023-11-16 21:18:19 -0300

    updating angular material

--- projeto_clinica/application/front/src/app/app.module.ts
+++ projeto_clinica/application/front/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponentsModule } from './main-components/main-components.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MainComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
